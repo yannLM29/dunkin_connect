574b4d49a96db15a3f978dedc4f0595e57fd3d71
Yann <yann.lemasson33@gmail.com>
2024-09-07 13:29:31 +0200

    Add player list to plugin

--- src/main/java/fr/yannlm29/ConnectionEventListener.java
+++ src/main/java/fr/yannlm29/ConnectionEventListener.java
@@ -1,5 +1,7 @@
 package fr.yannlm29;
 
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,13 +9,24 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionEventListener implements Listener{
+
+    private static final Logger LOGGER=Logger.getLogger("dunkin_connect");
+    private ConnectedPlayersList mPlayersList;
+
+
+    public ConnectionEventListener(ConnectedPlayersList inPlayerListRef) {
+        mPlayersList = inPlayerListRef;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        
+        if(!mPlayersList.addPlayer(event.getPlayer().getName())) {
+            LOGGER.warning("Tried to add player multiple times");
+        }
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        
+        mPlayersList.removePlayer(event.getPlayer().getName());
     }
 }
--- src/main/java/fr/yannlm29/Plugin.java
+++ src/main/java/fr/yannlm29/Plugin.java
@@ -9,10 +9,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Plugin extends JavaPlugin
 {
   private static final Logger LOGGER=Logger.getLogger("dunkin_connect");
+  ConnectedPlayersList mPlayersList;
+
+  public Plugin() {
+    super();
+    mPlayersList = new ConnectedPlayersList();
+  }
 
   public void onEnable()
   {
-    getServer().getPluginManager().registerEvents(new ConnectionEventListener(), this);
+    getServer().getPluginManager().registerEvents(new ConnectionEventListener(mPlayersList), this);
     getServer().getPluginManager().registerEvents(new GameEventListener(), this);
 
     LOGGER.info("dunkin_connect enabled");
